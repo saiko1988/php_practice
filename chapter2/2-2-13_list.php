c106cb9a961bb6076201bdc33b16741019cd18a0
saiko1988 <kos88thinline@gmail.com>
2021-11-09 23:58:58 +0900

    list()に関するコードを追加

---
+++ chapter2/2-2-13_list.php
@@ -0,0 +1,7 @@
+<?php
+$userData = [12345, 'Taro', 'Tokyo', 32];
+list($id, $name, $prefecture, $age) = $userData;
+var_dump($id . PHP_EOL);
+var_dump($name . PHP_EOL);
+var_dump($prefecture . PHP_EOL);
+var_dump($age . PHP_EOL);
