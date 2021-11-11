97e757a34d84f43cae2c3f9007d7072202246801
saiko1988 <kos88thinline@gmail.com>
2021-11-11 23:57:51 +0900

    関数のデフォルト引数についてのコードを追加

---
+++ chapter2/2-6-10_default_args.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+
+function greet(string $greeting = 'Hello.'): string
+{
+  return $greeting;
+}
+
+echo greet() . PHP_EOL; // Hello.
+echo greet('Good Afternoon.') . PHP_EOL;
+
+// デフォルト引数は右側にないとエラー
+function greet2(string $greeting = 'Hello', string $greeting2): string
+{
+  return "{$greeting} {$greeting2}";
+}
+
+// 2つ引数がないと関数を呼び出せないので、デフォルト引数の意味がない 
+echo greet2('Good Afternoon', 'World'); // Deprecated: Required parameter $greeting2 follows optional parameter $greeting in
