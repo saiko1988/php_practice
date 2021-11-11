837ea2431e8b70f328345f94537105db3be7217b
saiko1988 <kos88thinline@gmail.com>
2021-11-12 00:47:18 +0900

    変数・定数についての記述を追加

--- chapter2/2-6-10_default_args.php
+++ chapter2/2-6-10_default_args.php
@@ -17,3 +17,19 @@ function greet2(string $greeting = 'Hello', string $greeting2): string
 
 // 2つ引数がないと関数を呼び出せないので、デフォルト引数の意味がない 
 echo greet2('Good Afternoon', 'World'); // Deprecated: Required parameter $greeting2 follows optional parameter $greeting in
+
+// 変数をデフォルト値にすることはできない
+$default = 'World';
+function greet3(string $greeting, string $greeting2 = $default): string
+{
+  return "{$greeting} {$greeting2}";
+}
+echo greet3('Hello'); // Fatal error: Constant expression contains invalid operations in
+
+// 定数は設定できる
+define('GREETING2', 'World');
+function greet4(string $greeting, string $greeting2 = GREETING2): string
+{
+  return  "{$greeting} {$greeting2}";
+}
+echo greet4('Hello') . PHP_EOL;
