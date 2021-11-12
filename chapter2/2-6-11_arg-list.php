7a6d99c7dfdfd5d9ccaf0f46bf981653bf92458d
saiko1988 <kos88thinline@gmail.com>
2021-11-13 00:52:07 +0900

    可変超引数が最後の引数のみにしか使用できない旨の説明を追加

--- chapter2/2-6-11_arg-list.php
+++ chapter2/2-6-11_arg-list.php
@@ -12,3 +12,13 @@ function add(int ...$nums): int
 
 echo add(4,3,2,1);
 // echo add('a'); // エラー
+
+// 以下の`add2()`はエラー
+function add2(int ...$nums, string $str): string // Only the last parameter can be variadic
+{
+  $total = 0;
+  foreach ($nums as $num) {
+    $total += $num;
+  }
+  return "{$str}: {$total}";
+}
