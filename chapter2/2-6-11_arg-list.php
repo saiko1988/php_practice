c41335551e83a6ff9c3c3f3f005ae86957f5ba4a
saiko1988 <kos88thinline@gmail.com>
2021-11-13 00:59:52 +0900

    関数呼び出しに可変長引数を使用した場合の説明を追加

--- chapter2/2-6-11_arg-list.php
+++ chapter2/2-6-11_arg-list.php
@@ -14,11 +14,15 @@ echo add(4,3,2,1);
 // echo add('a'); // エラー
 
 // 以下の`add2()`はエラー
-function add2(int ...$nums, string $str): string // Only the last parameter can be variadic
-{
-  $total = 0;
-  foreach ($nums as $num) {
-    $total += $num;
-  }
-  return "{$str}: {$total}";
-}
+// function add2(int ...$nums, string $str): string // Only the last parameter can be variadic
+// {
+  // $total = 0;
+  // foreach ($nums as $num) {
+    // $total += $num;
+  // }
+  // return "{$str}: {$total}";
+// }
+
+// 関数呼び出し側で可変長引数を使うことができる
+$nums = [1,3,5,7]; // 配列が各要素の数値に展開される
+echo add(...$nums); // 16 
